5398ab3624ccd436f8d0715e8a2183115d1f9eca
Naman Anand <voidmemories@Namans-MacBook-Pro.local>
2023-05-29 16:43:47 +0530

    change headers

--- src/presentation/routes/app_router.js
+++ src/presentation/routes/app_router.js
@@ -1,4 +1,3 @@
-// @ts-nocheck
 import { Route, Routes } from "react-router";
 import {
   ADMIN_PAGE_ROUTE,
@@ -10,7 +9,7 @@ import LoginPage from "presentation/pages/login/login_page";
 import DashboardPage from "presentation/pages/dashboard/dashboard_page";
 import InputModal from "presentation/components/inputModal/inputModal";
 import AdminPage from "presentation/pages/admin/admin_page";
-import { ACCESS_TOKEN, CLIENT_ID } from "core/constants";
+import { ACCESS_TOKEN, APP_BASE_URL, CLIENT_ID } from "core/constants";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
@@ -26,20 +25,16 @@ function AppRouter(props) {
     var currentBrowserUrl = window.location.href;
 
     if (currentBrowserUrl.includes("access_token")) {
-      console.log(currentBrowserUrl);
       var myUrl = new URL(window.location.href.replace(/#/g, "?"));
       var access_token = myUrl.searchParams.get("access_token");
-      var id_token = myUrl.searchParams.get("id_token");
-      console.log(access_token);
-      console.log(id_token);
       isTokenValid(access_token).then((isValid) => {
-        if(!isValid){
+        if (!isValid) {
           navigateTo(LOGIN_PAGE_ROUTE);
           toast.error("Login failed. Please try again!");
-        }
-        else{
-          navigateTo(DASHBOARD_PAGE_ROUTE);
+        } else {
           toast.success("Login successful!");
+          localStorage.setItem(ACCESS_TOKEN, access_token);
+          navigateTo(DASHBOARD_PAGE_ROUTE);
         }
       });
     } else {
@@ -56,20 +51,29 @@ function AppRouter(props) {
   }, []);
 
   const isTokenValid = async (token) => {
+    if(token==null) return false;
+    
     return await axios
-      .get("https://www.googleapis.com/oauth2/v3/userinfo", {
-        headers: { Authorization: `Bearer ${token}` },
+      .get(`${APP_BASE_URL}/mds/api/v1/admin/ping`, {
+        headers: {
+          authMethod: "Cognito",
+          Token: token,
+        },
       })
       .then((res) => {
+        console.log(res);
         if (res.status == 200) {
           return true;
         }
-        return false;
+        else{
+          return false;
+        }
       })
-      .catch(() => {
+      .catch((e) => {
+        console.log(e);
         return false;
-      });
-  };
+      });}
+
 
   return (
     <Routes>
