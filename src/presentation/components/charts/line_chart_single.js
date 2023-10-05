7a12056b2c95055a25342325c5e64f83fa927d3b
Naman Anand <voidmemories@Namans-MacBook-Pro.local>
2023-10-05 12:37:27 +0530

    change graph type

--- src/presentation/components/charts/line_chart_single.js
+++ src/presentation/components/charts/line_chart_single.js
@@ -8,6 +8,8 @@ import {
   Tooltip,
   Legend,
   ResponsiveContainer,
+  BarChart,
+  Bar,
 } from "recharts";
 import { useState, useEffect } from "react";
 import { STROKE_COLORS_LIST } from "core/constants";
@@ -18,11 +20,11 @@ function AnalyticsLineChartSingle(props) {
 
   useEffect(() => {
     var tempData = [];
-    Object.keys(trends).map((key,index)=>{
-        tempData.push({
-            name:(new Date(key)).toLocaleString().substring(0,10),
-            DAU:trends[key]
-        })
+    Object.keys(trends).map((key, index) => {
+      tempData.push({
+        name: new Date(key).toLocaleString().substring(0, 10),
+        DAU: trends[key],
+      });
     });
 
     setData(tempData);
@@ -30,25 +32,26 @@ function AnalyticsLineChartSingle(props) {
 
   return (
     <ResponsiveContainer width="100%" height="100%">
-        <LineChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 64,
-            right: 72,
-            left: 20,
-            bottom: 64,
-          }}
-        >
-          <XAxis dataKey="name" dy={20}/>
-          <YAxis />
-          <Tooltip />
-          {/* <Legend /> */}
-          <Line type="monotone" dataKey="DAU" stroke="#8884d8" strokeWidth={3} activeDot={{ r: 4 }} dot={false} />
-        </LineChart>
-      </ResponsiveContainer>
+      <BarChart
+        data={data}
+        width={500}
+        height={300}
+        margin={{
+          top: 64,
+          right: 72,
+          left: 20,
+          bottom: 64,
+        }}
+      >
+        <XAxis dataKey="name" dy={20} />
+        <YAxis />
+        <Tooltip />
+        <XAxis dataKey="name" dy={20} />
+        <Bar dataKey="DAU" fill="#8884d8" />
+      </BarChart>
+    </ResponsiveContainer>
   );
 }
 
 export default AnalyticsLineChartSingle;
+
