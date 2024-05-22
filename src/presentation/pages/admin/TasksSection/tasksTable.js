331ad34a3b83435799d74ec89d34e1b8095278e9
Naman Anand <naman.anand.official@gmail.com>
2024-05-22 14:16:42 +0530

    working condition

--- src/presentation/pages/admin/TasksSection/tasksTable.js
+++ src/presentation/pages/admin/TasksSection/tasksTable.js
@@ -9,27 +9,19 @@ import TaskUpdate from "./taskUpdate";
 import { DEFAULT_TASK_NAME } from "../new_admin_page";
 import { fetchTaskFile } from "data/apis";
 import FileDownloadComponent from "presentation/components/FileDownload/fileDownload";
+import HoverText from "presentation/components/HoverText/hoverText";
 
 const taskTableView = {
     NEW_TASK_VIEW: 0,
     UPDATE_TASK_VIEW: 1
 }
 
-const taskActionColComponent = ({taskVersion}) => {
-    return (
-        <div className="taskActionCol">
-            <FileDownloadComponent fetchFunction={fetchTaskFile} fetchFuncData={{taskVersion: taskVersion}} fileName={DEFAULT_TASK_NAME} />
-        </div>
-        
-    )
-}
-
-const TasksTable = ({tasksDetails, allTagsList, updateTasksList, isUpdateTaskModalOpen, setIsUpdateTaskModalOpen}) => {
+const TasksTable = ({ tasksDetails, allTagsList, updateTasksList, isUpdateTaskModalOpen, setIsUpdateTaskModalOpen, setCodeEditorAsOpen }) => {
     const [tasksViewData, updateTasksViewData] = useState({
         headers: [
-            {text: 'Version'}, 
-            {text: 'Description'}, 
-            {text: 'Actions'}
+            { text: 'Version' },
+            { text: 'Description' },
+            { text: 'Actions' }
         ],
         body: [],
     });
@@ -38,7 +30,7 @@ const TasksTable = ({tasksDetails, allTagsList, updateTasksList, isUpdateTaskMod
     const [currentView, setCurrentView] = useState(null);
 
     useEffect(() => {
-        if(tasksDetails.hasOwnProperty(DEFAULT_TASK_NAME)) {
+        if (tasksDetails.hasOwnProperty(DEFAULT_TASK_NAME)) {
             setCurrentView(taskTableView.UPDATE_TASK_VIEW);
         } else {
             setCurrentView(taskTableView.NEW_TASK_VIEW);
@@ -61,29 +53,41 @@ const TasksTable = ({tasksDetails, allTagsList, updateTasksList, isUpdateTaskMod
         setIsUpdateTaskModalOpen(false);
     };
 
+    const taskActionColComponent = ({ taskVersion }) => {
+        return (
+            <div className="taskActionCol flex">
+                <FileDownloadComponent fetchFunction={fetchTaskFile} fetchFuncData={{ taskVersion: taskVersion }} fileName={DEFAULT_TASK_NAME} />
+                <HoverText onHoverText={"Edit Scipt"}>
+                    <img onClick={() => { setCodeEditorAsOpen(taskVersion); }} style={{ height: "20px", cursor: "pointer", marginLeft: "12px" }} src="/assets/icons/edit.svg"></img>
+                </HoverText>
+            </div>
+
+        )
+    }
+
     useEffect(() => {
         tasksViewData.body = [];
-        for(const taskName in tasksDetails) {
-            if(taskName != DEFAULT_TASK_NAME) continue;
-            for(const taskVersion in tasksDetails[taskName]['versions']) {
+        for (const taskName in tasksDetails) {
+            if (taskName != DEFAULT_TASK_NAME) continue;
+            for (const taskVersion in tasksDetails[taskName]['versions']) {
                 const tagsArray = [];
-                for(const tag in tasksDetails[taskName]['versionToTags'][taskVersion]) tagsArray.push(tag);
+                for (const tag in tasksDetails[taskName]['versionToTags'][taskVersion]) tagsArray.push(tag);
                 tasksViewData.body.push([
-                    {Component: TextOnlyComponent, data: {text: taskVersion, customStyle: {fontWeight: 500, color: '#494949', fontSize: '14px'}, highlightOnHover: true}}, 
-                    {Component: TextOnlyComponent, data: {text: tasksDetails[taskName]['versions'][taskVersion]['description'], customStyle: {color: '#74828F', fontWeight: 400, fontSize: '14px'}, highlightOnHover: true}},
-                    {Component: taskActionColComponent, data: {taskVersion: taskVersion}}
+                    { Component: TextOnlyComponent, data: { text: taskVersion, customStyle: { fontWeight: 500, color: '#494949', fontSize: '14px' }, highlightOnHover: true } },
+                    { Component: TextOnlyComponent, data: { text: tasksDetails[taskName]['versions'][taskVersion]['description'], customStyle: { color: '#74828F', fontWeight: 400, fontSize: '14px' }, highlightOnHover: true } },
+                    { Component: taskActionColComponent, data: { taskVersion: taskVersion } }
                 ]);
             }
             break;
         }
-        updateTasksViewData({...tasksViewData});
-        if(tasksDetails.hasOwnProperty(DEFAULT_TASK_NAME)) {
+        updateTasksViewData({ ...tasksViewData });
+        if (tasksDetails.hasOwnProperty(DEFAULT_TASK_NAME)) {
             setCurrentView(taskTableView.UPDATE_TASK_VIEW);
         } else {
             setCurrentView(taskTableView.NEW_TASK_VIEW);
         }
     }, [tasksDetails])
-    
+
     return (
         <>
             {
@@ -93,20 +97,20 @@ const TasksTable = ({tasksDetails, allTagsList, updateTasksList, isUpdateTaskMod
                         <div className="script-name">DEFAULT_SCRIPT</div>
                         <button onClick={openUpdateTaskModal} className="updateTaskButton">Update Workflow Script</button>
                     </div> */}
-                    {isUpdateTaskModalOpen && 
-                        <Modal isOpen={isUpdateTaskModalOpen} onClose={closeUpdateTaskModal} customStyle={{height: '654px'}}>
+                    {isUpdateTaskModalOpen &&
+                        <Modal isOpen={isUpdateTaskModalOpen} onClose={closeUpdateTaskModal} customStyle={{ height: '654px' }}>
                             <TaskUpdate allTagsList={allTagsList} onCompletion={closeUpdateTaskModal} updateTasksList={updateTasksList} />
                         </Modal>
                     }
-                    <Table data={tasksViewData}/>
+                    <Table data={tasksViewData} />
                 </div>
             }
             {
                 currentView == taskTableView.NEW_TASK_VIEW &&
                 <>
                     <button onClick={openNewTaskModal} className="uploadNewTaskButton">Upload Script</button>
-                    {isNewTaskModalOpen && 
-                        <Modal isOpen={isNewTaskModalOpen} onClose={closeNewTaskModal} customStyle={{height: '654px'}}>
+                    {isNewTaskModalOpen &&
+                        <Modal isOpen={isNewTaskModalOpen} onClose={closeNewTaskModal} customStyle={{ height: '654px' }}>
                             <TaskUpdate allTagsList={allTagsList} isNewTask={true} onCompletion={closeNewTaskModal} updateTasksList={updateTasksList} />
                         </Modal>
                     }
